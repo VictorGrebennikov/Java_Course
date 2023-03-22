bce8a1e9bfc8c9f570ed74552c74210ac6c0af9d
Victor Grebennikov <victorgrebennikov4@gmail.com>
2023-03-22 22:17:10 +0700

    HW task_2 for lesson_3 added

---
+++ homework/lesson_3/task_2.java
@@ -0,0 +1,28 @@
+package homework.lesson_3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class task_2 {
+    public static void main(String[] args) {
+        
+        // 2. Дан список ArrayList<String>. Удалить из него все строки, которые являются числами
+        // Пример: ArrayList<String> list = new ArrayList<>(List.of("string", "s", "5", "10", "-12", "my_value"));
+        // ["string", "s", "my_value"]
+
+        ArrayList<String> list1 = new ArrayList<>(List.of("string", "s", "5", "10", "-12", "my_value"));
+        textList1(list1);
+        System.out.println(list1);
+
+    }
+
+    private static void textList1(ArrayList<String> arr) {
+        for (int i = 0; i < arr.size(); i++) {
+            try {
+                Integer.parseInt(arr.get(i));
+                arr.remove(i--);
+            }
+            catch (Exception e){}
+        }
+    }
+}
